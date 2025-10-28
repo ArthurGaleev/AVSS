4fd0be868b0d8f111104aff0c3c12458c969ad94
setday <32331578+setday@users.noreply.github.com>
2025-10-28 19:25:15 +0300

    [ ADD ] Training gradient accumulation logic

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -13,7 +13,7 @@ class Trainer(BaseTrainer):
     Trainer class. Defines the logic of batch logging and processing.
     """
 
-    def process_batch(self, batch, metrics: MetricTracker):
+    def process_batch(self, batch, metrics: MetricTracker, zero_grad=True, update=True):
         """
         Run batch through the model, compute metrics, compute loss,
         and do training step (during training stage).
@@ -35,9 +35,7 @@ class Trainer(BaseTrainer):
         batch = self.move_batch_to_device(batch)
         batch = self.transform_batch(batch)  # transform batch on device -- faster
 
-        metric_funcs = self.metrics["inference"]
-        if self.is_train:
-            metric_funcs = self.metrics["train"]
+        if self.is_train and zero_grad:
             self.optimizer.zero_grad()
 
         outputs = self.model(**batch)
@@ -48,16 +46,19 @@ class Trainer(BaseTrainer):
 
         if self.is_train:
             batch["loss"].backward()  # sum of all losses is always called loss
-            self._clip_grad_norm()
-            self.optimizer.step()
-            if self.lr_scheduler is not None:
-                self.lr_scheduler.step()
+            
+            if update:
+                self._clip_grad_norm()
+                self.optimizer.step()
+                if self.lr_scheduler is not None:
+                    self.lr_scheduler.step()
 
         # update metrics for each loss (in case of multiple losses)
         for loss_name in self.config.writer.loss_names:
             metrics.update(loss_name, batch[loss_name].item())
 
-        for met in metric_funcs:
+        metrics_split = "train" if self.is_train else "inference"
+        for met in self.metrics[metrics_split]:
             metrics.update(met.name, met(**batch))
         return batch
 

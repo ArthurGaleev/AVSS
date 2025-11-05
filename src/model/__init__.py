d50dfcf687661989774b00725354e8d83c0b94ce
ArthurGaleev <arturiusgal@gmail.com>
2025-11-06 01:48:47 +0300

    add DPRNN_TasNet to init

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,8 @@
 from src.model.baseline_model import BaselineModel
+from src.model.dprnn_tasnet import DPRNN_TasNet
 from src.model.utils import reconstruct_spec_func
 
 __all__ = [
     "BaselineModel",
+    "DPRNN_TasNet"
 ]

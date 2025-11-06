[FIX] fixed a lot of tiny things, but the gradient flow problem wans't here :(

src/model/dprnn_tasnet.py
@@ -1,20 +1,10 @@
-import torch
-from torch import nn
 import torch.nn.functional as F
 import math
-
-
-class OneDConv(nn.Module):
-    def __init__(self):
-        super().__init__()
-    
-    def forward(self, x):
-        return
-
+from torch import nn
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, n_speakers):
         super().__init__()
 
         self.dconv1d_block = nn.Sequential(
@@ -29,16 +19,16 @@ class Encoder(nn.Module):
             nn.GroupNorm(num_groups=1, num_channels=hidden_dim), # TODO: add cumulative layer norm, because the task is causal
             )
         
-        self.res_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=input_dim, kernel_size=1)
-        self.out_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=input_dim, kernel_size=1)
+        self.res_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=hidden_dim*n_speakers, kernel_size=1)
+        self.out_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=1)
     
     def forward(self, audio_mix):
         x = self.dconv1d_block(audio_mix.unsqueeze(1))  # expected num_channels=1 in conv1d
 
         residual = self.res_conv(x)
-        out = self.out_conv(x) + audio_mix.unsqueeze(1)
+        x = self.out_conv(x) + audio_mix.unsqueeze(1)
         
-        return residual, out
+        return residual, x
     
 
 class Decoder(nn.Module):
@@ -46,25 +36,25 @@ class Decoder(nn.Module):
         super().__init__()
 
         self.dconv1d_block = nn.Sequential(
-            nn.ConvTranspose1d(in_channels=input_dim, out_channels=hidden_dim, kernel_size=1),
+            nn.ConvTranspose1d(in_channels=input_dim, out_channels=input_dim, kernel_size=1),
 
             nn.PReLU(),
-            nn.GroupNorm(num_groups=1, num_channels=hidden_dim), # TODO: add cumulative layer norm, because the task is causal
+            nn.GroupNorm(num_groups=1, num_channels=input_dim), # TODO: add cumulative layer norm, because the task is causal
 
-            nn.ConvTranspose1d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=3, padding=1, groups=hidden_dim),
+            nn.ConvTranspose1d(in_channels=input_dim, out_channels=input_dim, kernel_size=3, padding=1, groups=hidden_dim),
 
             nn.PReLU(),
-            nn.GroupNorm(num_groups=1, num_channels=hidden_dim), # TODO: add cumulative layer norm, because the task is causal
+            nn.GroupNorm(num_groups=1, num_channels=input_dim), # TODO: add cumulative layer norm, because the task is causal
             )
 
-        self.out_conv = nn.Conv1d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=1)
+        self.out_conv = nn.ConvTranspose1d(in_channels=input_dim, out_channels=hidden_dim, kernel_size=1)
     
     def forward(self, x):
-        out = self.dconv1d_block(x)
+        x = self.dconv1d_block(x)
 
-        out = self.out_conv(out) + x
+        x = self.out_conv(x)
         
-        return out
+        return x.squeeze(1)
 
 
 class Segmentation(nn.Module):
@@ -75,26 +65,23 @@ class Segmentation(nn.Module):
     
     def forward(self, x):
         # as in original DPRNN paper
-        # chunk_size = √(2*n_frames) = 2*p
-        p = int(math.sqrt(2*self.n_frames) // 2)
+        # chunk_size = √(2*n_frames)
+        chunk_size = int(math.sqrt(2 * self.n_frames))
 
         if self.overlap:
-            p_shift = p  # 50% overlap
+            hop_size = chunk_size // 2  # 50% overlap
         else:
-            p_shift = 2*p # no overlap
+            hop_size = chunk_size  # no overlap
 
-        chunks = []
-        chunks.append(F.pad(x[:, :, :p], (0, p)))  # add the first sample padded to chunk_size
-        
-        for i in range(p_shift, p * (self.n_frames // p - 1), p_shift):
-            chunks.append(x[:, :, i:(i+2*p)])  # add the next sample of chunk_size
+        # the first and last samples are padded to chunk_size
+        x = F.pad(x, (chunk_size//2, chunk_size - (self.n_frames % chunk_size)-1))
 
-        chunks.append(F.pad(x[:, :, (i+p):], (0, p - (self.n_frames % p)))) # add the last sample padded to chunk_size
+        x = x.unfold(2, chunk_size, hop_size)
         
         # need to permute stacked chunks of shape
-        # (n_chunks x batch_size x n_features x chunk_size) -> (batch_size x n_features x chunk_size x n_chunks)
-        return torch.stack(chunks).permute(1, 2, 3, 0)
-    
+        # (batch_size x n_features x n_chunks x chunk_size) -> (batch_size x n_features x chunk_size x n_chunks)
+        return x.permute(0, 1, 3, 2)
+
 
 class OverlapAdd(nn.Module):
     def __init__(self, n_frames, overlap=True):
@@ -103,29 +90,25 @@ class OverlapAdd(nn.Module):
         self.overlap = overlap
 
     def forward(self, x):
-        batch_size, n_features, chunk_size, n_chunks = x.shape
-
-        out = torch.zeros((batch_size, n_features, self.n_frames))
-
-        # as in original DPRNN paper
-        # chunk_size = √(2*n_frames) = 2*p
-        p = int(math.sqrt(2*self.n_frames) // 2)
-
-        overlap = True
-        if overlap:
-            p_shift = p  # 50% overlap
-        else:
-            p_shift = 2*p # no overlap
-
-        for i in range(0, n_chunks-1):
-            chunk_start = i * p_shift
-            out[:, :, chunk_start:(chunk_start+2*p)] += x[:, :, :, i].squeeze(-1)
-
-        # last chunk we cut a little bit to fit the out shape
-        chunk_start += p_shift
-        out[:, :, chunk_start:(chunk_start+2*p)] += x[:, :, :(self.n_frames % (2*p)), i].squeeze(-1)
+        batch, features, chunk_size, n_chunks = x.shape
         
-        return out
+        if self.overlap:
+            hop_size = chunk_size // 2
+        else:
+            hop_size = chunk_size
+        
+        output_length = (n_chunks - 1) * hop_size + chunk_size
+        
+        # Inverse to unfold
+        x = F.fold(
+            x.contiguous().view(batch, features * chunk_size, n_chunks),
+            output_size=(1, output_length),
+            kernel_size=(1, chunk_size),
+            stride=(1, hop_size)
+        )
+        
+        # reshape back and cut extra frames
+        return x.contiguous().view(batch, features, output_length)[:, :, :self.n_frames]
     
 
 class DPRNNBlock(nn.Module):
@@ -148,11 +131,11 @@ class DPRNNBlock(nn.Module):
         x = self.intra_norm(self.intra_fc(self.intra_rnn(x)[0])) + x
         
         # because inter_rnn is applied across the chunks
-        x = x.view(batch_size*chunk_size, n_chunks, n_features)
+        x = x.contiguous().view(batch_size*chunk_size, n_chunks, n_features)
         x = self.inter_norm(self.inter_fc(self.inter_rnn(x)[0])) + x
         
         # reshape back
-        return x.view(batch_size, n_features, chunk_size, n_chunks)
+        return x.contiguous().view(batch_size, n_features, chunk_size, n_chunks)
     
 
 class DPRNN(nn.Module):
@@ -200,11 +183,11 @@ class DPRNN_TasNet(nn.Module):
         self.enc_hidden_dim = enc_hidden_dim
         self.n_speakers = n_speakers
         
-        self.encoder = Encoder(input_dim=input_dim, hidden_dim=enc_hidden_dim)
+        self.encoder = Encoder(input_dim=input_dim, hidden_dim=enc_hidden_dim, n_speakers=n_speakers)
 
         self.sep_start = nn.Sequential(
-            nn.GroupNorm(num_groups=1, num_channels=input_dim),
-            nn.Conv1d(in_channels=input_dim, out_channels=enc_hidden_dim, kernel_size=1),
+            nn.GroupNorm(num_groups=1, num_channels=enc_hidden_dim),
+            nn.Conv1d(in_channels=enc_hidden_dim, out_channels=enc_hidden_dim, kernel_size=1),
         )
 
         self.separation = DPRNN(
@@ -223,19 +206,18 @@ class DPRNN_TasNet(nn.Module):
         
         self.decoder = Decoder(input_dim=enc_hidden_dim, hidden_dim=input_dim)
 
-
     def forward(self, audio_mix, **batch):
         residual, x = self.encoder(audio_mix)
         
         x = self.sep_start(x)
 
-        x = self.separation(x)*residual
+        x = self.separation(x)
 
-        x = self.sep_final(x).view(-1,  self.n_speakers, self.enc_hidden_dim, self.n_frames)
+        x = self.sep_final(x) * residual
+
+        x = x.contiguous().view(-1,  self.n_speakers, self.enc_hidden_dim, self.n_frames)
 
         return {
             "audio_pred_first": self.decoder(x[:, 0]),
-            "audio_pred_second": self.decoder(x[:, 1])
+            "audio_pred_second": self.decoder(x[:, 1]),
         }
-
-
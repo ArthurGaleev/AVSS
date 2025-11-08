6922ad61c4d2a4bfdb03e5511546f9d04cf0c94a
Torfinhell <you@example.com>
2025-11-09 02:30:43 +0300

    [FIX] SiSnr

--- src/loss/AudioSiSnr.py
+++ src/loss/AudioSiSnr.py
@@ -18,10 +18,12 @@ class AudioSiSnr(torch.nn.Module):
         audio_pred_second,
         **batch,
     ) -> Dict[str, Tensor]:
-        mse_loss_1 = si_snr_func(audio_pred_first, audio_first) + si_snr_func(
-            audio_pred_second, audio_second
+        mse_loss_1 = -(
+            si_snr_func().to(audio_pred_first.device)(audio_pred_first, audio_first)
+            + si_snr_func().to(audio_pred_first.device)(audio_pred_second, audio_second)
         )
-        mse_loss_2 = si_snr_func(audio_pred_second, audio_first) + si_snr_func(
-            audio_pred_first, audio_second
+        mse_loss_2 = -(
+            si_snr_func().to(audio_pred_first.device)(audio_pred_second, audio_first)
+            + si_snr_func().to(audio_pred_first.device)(audio_pred_first, audio_second)
         )
         return {"loss": mse_loss_1 if (mse_loss_1 < mse_loss_2) else mse_loss_2}

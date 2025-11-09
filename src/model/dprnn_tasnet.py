a9b9256aa4877a7faddad8538b019f52157c776b
Torfinhell <you@example.com>
2025-11-09 14:26:25 +0300

    init_utils

--- src/model/dprnn_tasnet.py
+++ src/model/dprnn_tasnet.py
@@ -179,15 +179,16 @@ class DPRNNBlock(nn.Module):
 
         # because inter_rnn is applied across the chunks
         x = x.view(batch_size, n_chunks, chunk_size, n_features)
-<<<<<<< HEAD
-        x = x.permute(0, 2, 1, 3).contiguous().view(batch_size * chunk_size, n_chunks, n_features)
-=======
         x = (
             x.permute(0, 2, 1, 3)
             .contiguous()
             .view(batch_size * chunk_size, n_chunks, n_features)
         )
->>>>>>> 34a313a (Changed permute in  Lstm)
+        x = (
+            x.permute(0, 2, 1, 3)
+            .contiguous()
+            .view(batch_size * chunk_size, n_chunks, n_features)
+        )
         x = self.inter_norm(self.inter_fc(self.inter_rnn(x)[0])) + x
 
         # reshape back

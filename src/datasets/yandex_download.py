4e7e793c5d28181882582d637536c46f12100c2e
Torfinhell <you@example.com>
2025-11-09 15:06:35 +0300

    [ADD] download full dataset

--- src/datasets/yandex_download.py
+++ src/datasets/yandex_download.py
@@ -9,10 +9,8 @@ from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.utils.io_utils import ROOT_PATH
 
 YANDEX_URL = {
-    "dla_dataset_small_a": {
-        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
-        "public_key": os.getenv("YANDEX_DISK_URL"),
-    }
+    "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
+    "public_key": os.getenv("YANDEX_DISK_URL"),
 }
 
 
@@ -29,13 +27,15 @@ class YandexDownload(CustomDirAudioDataset):
             data_dir = ROOT_PATH / "data" / "datasets"
         if not (data_dir / download_name / "audio" / part).exists():
             data_dir.mkdir(exist_ok=True, parents=True)
-            download_info = YANDEX_URL[download_name]
+            download_info = YANDEX_URL
             final_url = download_info["base_url"] + urlencode(
                 dict(public_key=download_info["public_key"])
             )
             response = requests.get(final_url)
             download_url = response.json()["href"]
+            print("Downloading zip data...")
             download_response = requests.get(download_url)
+            print("Successfully downloaded")
             zip = zipfile.ZipFile(io.BytesIO(download_response.content))
             zip.extractall(data_dir)
         data_dir = data_dir / download_name / "audio" / part

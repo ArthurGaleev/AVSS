635beb32000a514452ef1ff89b35067f3a15ad35
Torfinhell <you@example.com>
2025-11-13 19:00:06 +0300

    Added device

--- src/metrics/si_snr.py
+++ src/metrics/si_snr.py
@@ -1,7 +1,7 @@
 import torch
 
 from src.metrics.base_metric import BaseMetric
-from src.metrics.utils import si_snr_i, si_snr
+from src.metrics.utils import si_snr, si_snr_i
 
 
 class SiSnr(BaseMetric):
@@ -33,13 +33,13 @@ class SiSnr(BaseMetric):
     ):
         batch_size = audio_first.shape[0]
         loss1, loss2 = torch.zeros(batch_size), torch.zeros(batch_size)
-
+        self.metric_fn = self.metric_fn.to(audio_first.device)
         if self.compare in ["first", "average"]:
-             loss1 += self.metric_fn(audio_pred_first, audio_first, audio_mix)
-             loss2 += self.metric_fn(audio_pred_second, audio_first, audio_mix)
+            loss1 += self.metric_fn(audio_pred_first, audio_first, audio_mix)
+            loss2 += self.metric_fn(audio_pred_second, audio_first, audio_mix)
         if self.compare in ["second", "average"]:
-             loss1 += self.metric_fn(audio_pred_first, audio_second, audio_mix)
-             loss2 += self.metric_fn(audio_pred_second, audio_second, audio_mix)
+            loss1 += self.metric_fn(audio_pred_first, audio_second, audio_mix)
+            loss2 += self.metric_fn(audio_pred_second, audio_second, audio_mix)
         si_snri = torch.max(loss1, loss2)[0].mean()
 
         norm_coeff = 2 if self.compare == "average" else 1

2ee2eeeed76da7611b585e70d1a27d79f35dc187
ArthurGaleev <arturiusgal@gmail.com>
2025-12-14 14:43:30 +0300

    improve consistency in downloading by Ya + optimize config

--- src/datasets/yandex_download.py
+++ src/datasets/yandex_download.py
@@ -9,25 +9,6 @@ import requests
 from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.utils.io_utils import ROOT_PATH
 
-YANDEX_URL = {
-    "dla_dataset": {
-        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
-        "public_key": os.getenv("YANDEX_DISK_URL"),
-    },
-    "dla_dataset_small_a": {
-        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
-        "public_key": os.getenv("YANDEX_DISK_URL"),
-    },
-    "dla_dataset_small_av": {
-        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
-        "public_key": os.getenv("YANDEX_DISK_URL"),
-    },
-    "dla_dataset_onebatch_a": {
-        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
-        "public_key": os.getenv("YANDEX_DISK_URL"),
-    },
-}
-
 
 class YandexDownload(CustomDirAudioDataset):
     def __init__(
@@ -41,18 +22,28 @@ class YandexDownload(CustomDirAudioDataset):
         if data_dir is None:
             data_dir = ROOT_PATH / "data" / "datasets"
             if not (data_dir / download_name / "audio" / part).exists():
-                data_dir.mkdir(exist_ok=True, parents=True)
-                download_info = YANDEX_URL[download_name]
-                final_url = download_info["base_url"] + urlencode(
-                    dict(public_key=download_info["public_key"])
-                )
-                response = requests.get(final_url)
-                download_url = response.json()["href"]
-                print("Downloading zip data...")
-                download_response = requests.get(download_url)
-                print("Successfully downloaded")
-                zip = zipfile.ZipFile(io.BytesIO(download_response.content))
-                zip.extractall(data_dir)
+                try:
+                    data_dir.mkdir(exist_ok=True, parents=True)
+                    download_info = {
+                        "base_url": "https://cloud-api.yandex.net/v1/disk/public/resources/download?",
+                        "public_key": os.getenv("YANDEX_DISK_URL"),
+                    }
+                    final_url = download_info["base_url"] + urlencode(
+                        dict(public_key=download_info["public_key"])
+                    )
+                    response = requests.get(final_url)
+                    download_url = response.json()["href"]
+                    print("Downloading zip data...")
+                    download_response = requests.get(download_url)
+                    print("Successfully downloaded")
+                    zip = zipfile.ZipFile(io.BytesIO(download_response.content))
+                    zip.extractall(data_dir)
+                except requests.exceptions.ConnectTimeout as e:
+                    print(f"Connection timed out: {e}")
+                except requests.exceptions.ReadTimeout as e:
+                    print(f"Read timed out: {e}")
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred: {e}")
             data_dir = data_dir / download_name
         else:
             data_dir = Path(data_dir)
